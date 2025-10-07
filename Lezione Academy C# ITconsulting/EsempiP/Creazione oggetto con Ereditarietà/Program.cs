3d8b721012f6d9a321cc4be2d9b3717735605c80
Fabio <137040853+Egy98@users.noreply.github.com>
2025-10-07 15:11:25 +0200

    Add files via upload

---
+++ Lezione Academy C# ITconsulting/EsempiP/Creazione oggetto con Ereditarietà/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// ** EREDITARIETA' **
+
+//Classe padre
+public class Animale
+{
+    public void FaiVerso()
+    {
+        Console.WriteLine($"L'animale fa un verso.");
+    }
+}
+
+//Classe derivata, viene usato :
+public class Cane : Animale
+{
+    public void Scodinzola()
+    {
+        Console.WriteLine($"Il cane scodinzola.");
+
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        //Creazione di un oggetto (istanza della classe Cane) che sarebbe la derivata
+        Cane mioCane = new Cane();
+
+        //Assegnazione dei valori alle proprietà
+        mioCane.FaiVerso();
+        mioCane.Scodinzola();
+    }
+}
