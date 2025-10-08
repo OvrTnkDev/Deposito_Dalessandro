2f36c245a3dbf45cdca26c0ed6ec18041fcd1b44
Fabio <137040853+Egy98@users.noreply.github.com>
2025-10-08 13:00:20 +0200

    Add files via upload

---
+++ Lezione Academy C# ITconsulting/EsempiP/Creazione oggetto con Incapsulamento/Program.cs
@@ -0,0 +1,40 @@
+﻿public class ContoBancario
+{
+    // Campo privato (non accessibile direttamente dall esterno)
+    private double saldo;
+    public string nomeContoBancario;
+    // Proprietà per accedere al saldo in modo controllato
+    public double Saldo
+    {
+        get { return saldo; } // permette 1a lettura del saldo
+        set
+        {
+            if (value >= 0)    // solo valori validi
+                saldo = value;
+        }
+    }
+
+    public string NomeContoBancario
+    {
+        { get return nomeContoBancario;
+         private set; }
+    }
+
+    public class Programma
+    {
+        public static void Main()
+        {
+            ContoBancario conto = new ContoBancario();
+
+            // conto.NomeContoBancario = "Intesa SanPaolo";
+            // Console.WriteLine(conto.NomeContoBancario);
+
+            conto.Saldo = 1560.90;
+            Console.WriteLine(conto.Saldo);
+
+            conto.Saldo -= 510;
+            Console.WriteLine(conto.Saldo);
+        }
+    }
+}
+
