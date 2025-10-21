7c1045f883afab59702d762e9859c5e16d740274
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-21 15:16:04 +0200

    Method Injection

---
+++ Lezione Academy C# ITconsulting/Corso C# 21-10-25 Pomeriggio/MethodInjectionEsercizio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public interface INotifier
+{
+void Notify(string message);
+}
+
+public class SmsNotifier : INotifier
+{
+public void Notify(string message)
+{
+Console.WriteLine($"[Invio SMS]: {message}");
+}
+}
+
+public class AlertService
+{
+    public void SendAlert(string user, INotifier notifier)
+    {
+        notifier.Notify($"Ciao {user}, hai una nuovo messaggio!");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        INotifier notifier = new SmsNotifier();
+        var service = new AlertService();
+        service.SendAlert("Fabio", notifier);
+    }
+}
