40db6f8abbfbc9063bed688f1242d740cc2f526f
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-21 10:51:00 +0200

    DependecyInjection esercizio 1

---
+++ Lezione Academy C# ITconsulting/Corso C# 21-10-25 Mattina/DependencyInjectionEx1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message) => Console.WriteLine($"[LOG]: {message}");
+}
+
+public class Printer
+{
+    public ILogger? Logger { get; set; }
+
+    public void Print(string m)
+    {
+        if (Logger == null)
+        {
+            Console.WriteLine($"{m}");
+            return;
+        }
+
+        Logger.Log(m);
+        Console.WriteLine($"print: {m}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] Args)
+    {
+        var print = new Printer();
+        print.Print("Hello, World! senza logger");
+        print.Logger = new ConsoleLogger();
+        print.Print("Hello, World! con logger");
+    }
+}
