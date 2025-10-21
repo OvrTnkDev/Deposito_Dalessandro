432e66cdfdcd268198f55306282c468b391ccaba
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-21 16:24:49 +0200

    enum esempio

--- Lezione Academy C# ITconsulting/Corso C# 21-10-25 Pomeriggio/EnumEs/Program.cs
+++ Lezione Academy C# ITconsulting/Corso C# 21-10-25 Pomeriggio/EnumEs/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+public class Nemico : MonoBehaviour
+{
+    public enum TipoNemico { Zombie, Robot, Fantasma }
+    [SerializeField]
+    private TipoNemico tipo;
+    private void Start()
+    {
+        switch (tipo)
+        {
+            case TipoNemico.Zombie:
+                Debug.Log("Questo è uno zombie");
+                break;
+            case TipoNemico.Robot:
+                Debug.Log("Questo è un robot");
+                break;
+            case TipoNemico.Fantasma:
+                Debug.Log("Questo è un fantasma");
+            break;
+        }
+    }
+}
