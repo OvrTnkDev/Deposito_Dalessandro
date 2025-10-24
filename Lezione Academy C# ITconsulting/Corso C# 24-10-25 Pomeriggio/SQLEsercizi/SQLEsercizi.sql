73a57e4974f6bd6e4bbebf3cde93934f2fe90c02
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-24 16:26:00 +0200

    + Esercizio gruppo

--- Lezione Academy C# ITconsulting/Corso C# 24-10-25 Pomeriggio/SQLEsercizi/SQLEsercizi.sql
+++ Lezione Academy C# ITconsulting/Corso C# 24-10-25 Pomeriggio/SQLEsercizi/SQLEsercizi.sql
@@ -44,3 +44,43 @@ VALUES
     FROM world.country
     WHERE Continent = "Antarctica"
     ORDER BY Continent ASC;*/
+    
+    -- ESERCIZIO DI GRUPPO
+    -- CREAZIONE DB
+	CREATE DATABASE IF NOT EXISTS Libreria;
+	USE Libreria;
+    
+    -- CREAZIONE TABELLA
+	CREATE TABLE IF NOT EXISTS Libri (
+		ID INT AUTO_INCREMENT PRIMARY KEY,
+		Nome VARCHAR(100) NOT NULL,
+		Autore VARCHAR(100) NOT NULL,
+        Genere VARCHAR (50) NOT NULL,
+        Prezzo DECIMAL (5,2),
+		AnnoPubblicazione YEAR
+	);
+    
+    -- INSERIMENTO DATI
+	INSERT INTO Libri (Nome, Autore, Genere, Prezzo, AnnoPubblicazione) VALUES
+	('Il nome della rosa', 'Umberto Eco', 'Giallo storico', 14.90, 1980),
+	('1984', 'George Orwell', 'Distopico', 12.50, 1949),
+	('Cent’anni di solitudine', 'Gabriel García Márquez', 'Realismo magico', 16.00, 1967),
+	('Orgoglio e pregiudizio', 'Jane Austen', 'Romanzo', 10.90, 1813),
+	('Il signore degli anelli', 'J.R.R. Tolkien', 'Fantasy', 24.99, 1954),
+	('La coscienza di Zeno', 'Italo Svevo', 'Letteratura italiana', 11.50, 1923),
+	('Harry Potter e la pietra filosofale', 'J.K. Rowling', 'Fantasy', 18.00, 1997),
+	('Il codice Da Vinci', 'Dan Brown', 'Thriller', 13.90, 2003),
+	('Sapiens: Da animali a dèi', 'Yuval Noah Harari', 'Saggio', 19.50, 2011),
+	('La strada', 'Cormac McCarthy', 'Post-apocalittico', 9.99, 2006);
+    
+    -- CREAZIONE 1 QUERY
+    SELECT Genere, count(Nome) "Conteggio Libro", avg(Prezzo) "Prezzo Medio"
+    FROM Libreria.Libri
+    GROUP BY Genere
+    ORDER BY Genere;
+    
+    -- CREAZIONE 2 QUERY
+    SELECT * FROM Libreria.Libri
+    WHERE AnnoPubblicazione > 2010
+    ORDER BY Anno DESC, Prezzo
+    
